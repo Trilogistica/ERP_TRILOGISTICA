bc08968bc3d3ebfd2430420b9540fed1b2de6652
Gustavo <cru22779@uvg.edu.gt>
2025-01-23 17:56:32 -0600

    Fix: Dockerfile + entrypoint

--- erp/entrypoint.sh
+++ erp/entrypoint.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
-
-# Mostrar arte ASCII al inicio
 echo "
-  ___             _             
- |  __ \           | |            
- | |  | | _   _| | __ _ _ 
- | |  | |/ _ \ / _| |/ / _ | '_| 
- | |_| | () | (_|   |  _| |   
- |__/ \_/ \_||\\_||   
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢰⣶⣶⣶⠀⣶⣶⣶⣶⠀⢰⣶⣶⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⣿⣿⣿⣿⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢠⣤⣤⣤⠀⢠⣤⣤⣤⠀⣤⣤⣤⣤⠀⢠⣤⣤⣤⠀⣰⣿⣿⣦⡀⠀⠀⠀⠀
+⠀⢸⣿⣿⣿⠀⢸⣿⣿⣿⠀⣿⣿⣿⣿⠀⢸⣿⣿⣿⠀⣿⣿⠹⣿⣷⣀⠀⠀⠀
+⠀⠘⠛⠛⠛⠀⠘⠛⠛⠛⠀⠛⠛⠛⠛⠀⠘⠛⠛⠛⢀⣿⣿⡀⠙⠿⠿⣿⣶⣆
+⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⠟⢁⣤⣴⣶⣾⡿⠋
+⣿⣿⣿⣛⣛⣛⣛⣿⣟⣛⣛⣻⣿⣟⣛⣛⣻⣿⣟⣋⣉⣠⣤⣾⣿⣟⣻⣍⠀⠀
+⢹⣿⣿⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠋⠀⠀⠀⠀⠀
+⠈⢻⣿⣿⣿⡿⠿⠿⠿⠛⠉⠀⠀⠀⠀⠀⢀⣠⣴⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠙⢿⣿⣿⣶⣦⣤⣤⣤⣤⣤⣴⣶⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⠿⠿⠿⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 "
 
-# Verifica si `requirements.txt` existe
+# Verificar si `requirements.txt` existe
 if [ ! -f requirements.txt ]; then
     echo "❌ requirements.txt no encontrado. Creando un archivo vacío..."
     touch requirements.txt
 fi
 
-# Instala dependencias desde requirements.txt
+# Instalar dependencias desde requirements.txt
 echo -e "\nInstalando dependencias desde requirements.txt..."
-pip install --no-cache-dir -r requirements.txt
+pip-sync requirements.txt || pip install --no-cache-dir -r requirements.txt
 
-# Asegúrate de que las dependencias estén sincronizadas cada 5 minutos
-function sync_requirements {
+# Función para actualizar `requirements.txt`
+function monitor_packages {
     while true; do
-        echo -e "\nActualizando requirements.txt con dependencias instaladas..."
+        echo -e "\nSincronizando requirements.txt con dependencias actuales..."
         pip freeze > requirements.txt
         echo "✅ requirements.txt actualizado correctamente."
         sleep 300
     done
 }
 
-# Ejecutar sincronización de dependencias en segundo plano
-sync_requirements &
-
+monitor_packages &
 echo -e "\nIniciando la aplicación..."
 exec "$@"
 
