ff102a857bb6261084d370b93fc4fa054a7cd25f
cor22982 <cor22982@uvg.edu.gt>
2025-01-25 17:55:45 -0600

    feat: Añadir gastos y modelos

--- erp/modulos_desarrollados/gastos/models/models.py
+++ erp/modulos_desarrollados/gastos/models/models.py
@@ -6,8 +6,18 @@ from odoo import models, fields, api
 class gasto(models.Model):
       _name = 'gastos.gasto'
 #     _description = 'gastos.gastos'
-      name = fields.Char(string="Nombre")
-      value = fields.Integer()
+      name = fields.Char(string="Descripcion")
+      value = fields.Integer(string="Valor")
+      expense_date = fields.Date(string="Fecha de Gasto")
+
+      @api.model
+      def create_gasto(self):
+        for record in self:
+            self.create({'name': 'Nuevo Gasto', 'value': 0.0})
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'reload',
+            }
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
