4c43dfd3856bec7dd5d176f0cf6a292368ed52dc
cor22982 <cor22982@uvg.edu.gt>
2025-02-17 14:46:12 -0600

    feat: Add excel report

--- erp/odoo/modulos_desarrollados/gastos/controllers/controllers.py
+++ erp/odoo/modulos_desarrollados/gastos/controllers/controllers.py
@@ -1,22 +1,53 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+# # -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import Response
+from odoo.http import request
+# import pandas as pd
+import io
+import json
+import xlsxwriter
 
+class GastoController(http.Controller):
 
-# class Gastos(http.Controller):
-#     @http.route('/gastos/gastos', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/descargar_excel', auth='public', type='http', methods=['GET'])
+    def descargar_excel(self, **kw):
+        output = io.BytesIO()
 
-#     @http.route('/gastos/gastos/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('gastos.listing', {
-#             'root': '/gastos/gastos',
-#             'objects': http.request.env['gastos.gastos'].search([]),
-#         })
+        # Crear el archivo Excel
+        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+        worksheet = workbook.add_worksheet('Hoja1')
 
-#     @http.route('/gastos/gastos/objects/<model("gastos.gastos"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('gastos.object', {
-#             'object': obj
-#         })
+        # Agregar datos manualmente
+        data = [
+            ['Columna 1', 'Columna 2'],
+            [1, 'A'],
+            [2, 'B'],
+            [3, 'C']
+        ]
 
+        for row_num, row_data in enumerate(data):
+            for col_num, cell_data in enumerate(row_data):
+                worksheet.write(row_num, col_num, cell_data)
+
+        workbook.close()
+        output.seek(0)
+
+        return Response(
+            output.getvalue(),
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            headers=[("Content-Disposition", "attachment; filename=archivo.xlsx")]
+        )
+    @http.route('/gastos/suma_total', auth='public', type='http')
+    def suma_total_gastos(self, **kw):
+        # Obtener todos los gastos con estado 'pagado'
+        total = request.env['gastos.gasto'].search([('status', '=', 'pagado')]).mapped('value')
+        total_isr =  request.env['gastos.gasto'].search([('status', '=', 'pagado')]).mapped('isr')
+        # Sumar todos los valores de los gastos
+        suma_total = sum(total)
+        suma_isr = sum(total_isr)
+        
+        # Crear la respuesta JSON
+        return Response(
+            json.dumps({'suma_total': suma_total, 'suma_isr': suma_isr}),
+            content_type='application/json',
+        )
